4ec17aaf60c01979e629a766c635a9f24a01d452
Dmytro Kuzmin <374645@mail.muni.cz>
2021-11-15 13:58:57 +0100

    KX-2258 - Add sample module that configures contact custom fields

---
+++ src/Kentico.Xperience.Intercom.Admin.SampleModule/SampleModule.cs
@@ -0,0 +1,51 @@
+﻿using CMS;
+using CMS.DataEngine;
+
+using Kentico.Xperience.Intercom.Admin.SampleModule;
+
+[assembly: AssemblyDiscoverable]
+
+[assembly: RegisterModule(typeof(SampleModule))]
+
+namespace Kentico.Xperience.Intercom.Admin.SampleModule
+{
+    /// <summary>
+    /// Represents sample module that handles <see cref="IntercomEvents.UpdateContact"/> event to configure custom fields.
+    /// </summary>
+    public class SampleModule : Module
+    {
+        public const string NAME = "Kentico.Xperience.Intercom.Sample";
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SampleModule"/> class.
+        /// </summary>
+        public SampleModule() : base(NAME)
+        {
+        }
+
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            IntercomEvents.UpdateContact.Execute += UpdateContactCustomFields;
+        }
+
+
+        private static void UpdateContactCustomFields(object sender, IntercomUpdateContactEventArgs e)
+        {
+            var contact = e.Contact;
+
+            if (e.ContactData.TryGetValue("ContactCafeOwner", out var ownerValue))
+            {
+                contact.SetValue("ContactCafeOwner", (string)ownerValue);
+            }
+
+            if (e.ContactData.TryGetValue("ContactCoffeePreference", out var coffePreference))
+            {
+                contact.SetValue("ContactCoffeePreference", (string)coffePreference);
+            }
+        }
+    }
+}
