2f6ce052c06300a6250762c31183485cb2b48e3f
Dmytro Kuzmin <374645@mail.muni.cz>
2021-11-15 12:15:21 +0100

    KX-2258 - Add live site modules with HTMLHelpers

---
+++ src/Kentico.Xperience.Intercom.AspNetCore/HtmlHelpers/HtmlHelperExtensions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Web;
+
+using CMS.ContactManagement;
+using CMS.DataEngine;
+using CMS.SiteProvider;
+
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Kentico.Xperience.Intercom
+{
+    public static class HtmlHelperExtensions
+    {
+        /// <summary>
+        /// Registers Intercom scripts according to the current site's Intercom configuration.
+        /// </summary>
+        public static IHtmlContent IntercomScripts(this IHtmlHelper _)
+        {
+            var currentSite = SiteContext.CurrentSite;
+            if (currentSite == null)
+            {
+                new HtmlContentBuilder();
+            }
+
+            var intercomEnabled = SettingsKeyInfoProvider.GetBoolValue($"{SiteContext.CurrentSiteName}.CMSIntercomEnabled");
+            var intercomAppID = SettingsKeyInfoProvider.GetValue($"{SiteContext.CurrentSiteName}.CMSIntercomAppID");
+
+            if (!intercomEnabled || String.IsNullOrEmpty(intercomAppID))
+            {
+                new HtmlContentBuilder();
+            }
+
+            var currentContact = ContactManagementContext.CurrentContact;
+            string contactName = null;
+
+            if (currentContact != null && !currentContact.ContactLastName.StartsWith(ContactHelper.ANONYMOUS, StringComparison.Ordinal))
+            {
+                contactName = $"{currentContact.ContactFirstName} {currentContact.ContactLastName}".Trim();
+            }
+
+            var generatedHtml = new HtmlContentBuilder().AppendFormat(@"<script>
+window.intercomSettings = {{
+    app_id: ""{0}""", intercomAppID);
+
+            if (currentContact != null)
+            {
+                generatedHtml.AppendFormat(@",
+    user_id: ""{0}""", currentContact.ContactGUID.ToString());
+            }
+
+            if (!String.IsNullOrEmpty(contactName))
+            {
+                generatedHtml.AppendFormat(@",
+    name: ""{0}""", HttpUtility.JavaScriptStringEncode(contactName));
+            }
+
+            if (!String.IsNullOrEmpty(currentContact?.ContactEmail))
+            {
+                generatedHtml.AppendFormat(@",
+    email: ""{0}""", HttpUtility.JavaScriptStringEncode(currentContact.ContactEmail));
+            }
+
+            var identityVerificationSecret = SettingsKeyInfoProvider.GetValue($"{SiteContext.CurrentSiteName}.CMSIntercomIdentityVerificationSecret");
+            if (currentContact != null && !String.IsNullOrEmpty(identityVerificationSecret))
+            {
+                generatedHtml.AppendFormat(@",
+    user_hash: ""{0}""", SecurityMethods.CalculateHash(currentContact.ContactGUID.ToString(), identityVerificationSecret));
+            }
+
+
+            generatedHtml.AppendLine("};");
+
+            generatedHtml.AppendFormat("(function () {{ var w = window; var ic = w.Intercom; if (typeof ic === \"function\") {{ ic('reattach_activator'); ic('update', w.intercomSettings); }} else {{ var d = document; var i = function () {{ i.c(arguments); }}; i.q = []; i.c = function (args) {{ i.q.push(args); }}; w.Intercom = i; var l = function () {{ var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/{0}'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); }}; if (w.attachEvent) {{ w.attachEvent('onload', l); }} else {{ w.addEventListener('load', l, false); }} }} }})();", intercomAppID);
+
+            generatedHtml.AppendHtmlLine("</script>");
+
+            return generatedHtml;
+        }
+    }
+}
---
+++ src/Kentico.Xperience.Intercom.Mvc5/Security/SecurityMethods.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace Kentico.Xperience.Intercom
+{
+    internal static class SecurityMethods
+    {
+        public static string CalculateHash(string message, string secret)
+        {
+            var encoding = new ASCIIEncoding();
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(message);
+            using (var hmacsha1 = new HMACSHA1(keyByte))
+            {
+                byte[] hashmessage = hmacsha1.ComputeHash(messageBytes);
+                var sb = new StringBuilder();
+                for (var i = 0; i <= hashmessage.Length - 1; i++)
+                {
+                    sb.Append(hashmessage[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
